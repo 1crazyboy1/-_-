62c93ce61b85e054306c71cbac81750e556247fe
110 <471746662@qq.com>
2022-07-06 17:01:28 +0800

    FINESH

--- 委托_事件/Form1.cs
+++ 委托_事件/Form1.cs
@@ -16,5 +16,55 @@ namespace 委托_事件
         {
             InitializeComponent();
         }
+
+        class MyEvent
+        {
+            public event MakeHotMachineHandler temperatureChangeEvent;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyEvent eve = new MyEvent();
+            eve.temperatureChangeEvent += MakeHotMachine;
+            if (string.IsNullOrEmpty(textBox1.Text.Trim()))
+            {
+                MessageBox.Show("请输入水温");
+                return;
+            }
+            MakeHotMachine(Convert.ToInt32(textBox1.Text.Trim()));
+            TvSHOW(Convert.ToInt32(textBox1.Text.Trim()));
+
+
+        }
+
+        public delegate string MakeHotMachineHandler(int temprature);
+
+        public string  MakeHotMachine(int temprature)
+        {
+            if (temprature>=95)
+            {
+                label4.Text = "110";
+            }
+            else
+            {
+                label4.Text = temprature.ToString();
+            }
+            return label3.Text;
+        }
+
+        public string TvSHOW(int temprature)
+        {
+            if (temprature >= 95)
+            {
+                label5.Text = "110";
+            }
+            else
+            {
+                label5.Text = temprature.ToString();
+            }
+            return label5.Text;
+        }
+
+
     }
 }
